c14ead0d45ce713f82bf2c3b13b1b39c68e81e4b
1ucky40nc3 <louisnwendler@gmail.com>
2021-04-04 21:32:16 +0200

    unit tests for entities on the chessboard

---
+++ test/test_entity.py
@@ -0,0 +1,26 @@
+def test_entity():
+    pass
+
+def test_empty():
+    pass
+
+def test_piece():
+    pass
+
+def test_pawn():
+    pass
+
+def test_knight():
+    pass
+
+def test_bishop():
+    pass
+
+def test_rook():
+    pass
+
+def test_queen():
+    pass
+
+def test_king():
+    pass
