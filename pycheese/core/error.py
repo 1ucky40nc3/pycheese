04cef140db00cfe71cb33ca0e7e90f48194b2301
1ucky40nc3 <louisnwendler@gmail.com>
2021-04-07 00:08:58 +0200

    added NotInPlayersPossesionException

---
+++ pycheese/core/error.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Custom errors.
+
+This module contains code to be able to raise custom
+errors and exceptios by the package in the sustainable way.
+"""
+
+
+class NotInPlayersPossionException(Exception):
+    pass
