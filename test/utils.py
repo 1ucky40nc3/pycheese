55cbd2ec7f9a8d79270417287c37273b29a18208
1ucky40nc3 <louisnwendler@gmail.com>
2021-05-10 21:15:08 +0200

    moved functions from test_board

--- test/utils.py
+++ test/utils.py
@@ -46,3 +46,57 @@ def assert_obj_func(obj: object, func: str, param: Union[List[Any], None], targe
         assert getattr(obj, func)(*param) == target
     else:
         assert getattr(obj, func)() == target
+
+
+def white_pawns() -> List[Type[Pawn]]:
+    """Return a list of all of white's pawns."""
+    return [Pawn((i, 6), "white") for i in range(8)]
+
+
+def black_pawns() -> List[Type[Pawn]]:
+    """Return a list of all of black's pawns."""
+    return [Pawn((i, 1), "black") for i in range(8)]
+
+
+def white_pieces() -> List[Type[Piece]]:
+    """Return a list of all of white's pieces. (Note: Pawns exclusive.)"""
+    return [
+        Rook((0, 7), "white"), 
+        Knight((1, 7), "white"), 
+        Bishop((2, 7), "white"), 
+        Queen((3, 7), "white"), 
+        King((4, 7), "white"), 
+        Bishop((5, 7), "white"), 
+        Knight((6, 7), "white"), 
+        Rook((7, 7), "white"),
+    ]
+
+
+def black_pieces() -> List[Type[Piece]]:
+    """Return a list of all of black's pieces. (Note: Pawns exclusive.)"""
+    return [
+        Rook((0, 0), "black"), 
+        Knight((1, 0), "black"), 
+        Bishop((2, 0), "black"), 
+        Queen((3, 0), "black"), 
+        King((4, 0), "black"), 
+        Bishop((5, 0), "black"), 
+        Knight((6, 0), "black"), 
+        Rook((7, 0), "black"),
+    ]
+
+
+def initial_board() -> List[List[Type[Entity]]]:
+    """Create a nested list of Entitys that represents the chessboard."""
+    board = []
+
+    board.append(black_pieces())
+    board.append(black_pawns())
+
+    for i in range(4):
+        board.append([Empty((j, i + 2)) for j in range(8)])
+
+    board.append(white_pawns())
+    board.append(white_pieces())
+    
+    return board
