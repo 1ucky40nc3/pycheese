c072d16496135761684fc37764c7c830c29dc9c9
1ucky40nc3 <louisnwendler@gmail.com>
2021-05-17 16:44:37 +0200

    fixed utils and added tests

--- pycheese/core/utils.py
+++ pycheese/core/utils.py
@@ -6,112 +6,8 @@ of the package in the sustainable way.
 """
 
 
-from typing import Type
-from typing import List
-from typing import Tuple
 from typing import Union
-
-from pycheese.core.entity import Entity
-from pycheese.core.entity import Empty
-from pycheese.core.entity import Piece
-from pycheese.core.entity import Pawn
-from pycheese.core.entity import Knight
-from pycheese.core.entity import Bishop
-from pycheese.core.entity import Rook
-from pycheese.core.entity import Queen
-from pycheese.core.entity import King
-
-from pycheese.core.error import NotWhitelistedException
-
-
-def initial_board(self) -> List[List[Type[Entity]]]:
-    """Create a nested list of Entitys that represents the chess board.
-
-    Note:
-        The chess board is build with the position 
-        'a8' at the coordinate (0, 0) (h1 --> (7, 7)).
-        Reminder: Coordinates have to be translated!
-        This function is called in the constructor.
-
-    Example:
-        >>> from pycheese.core.board import Board
-        >>> from pycheese.core.utils import initial_board
-        >>> board = Board()
-        >>> board.set(initial_board())
-        >>> print(board.show())
-        ♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜
-        ♟︎ ♟︎ ♟︎ ♟︎ ♟︎ ♟︎ ♟︎ ♟︎ 
-        ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ ⊡
-        ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ ⊡
-        ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ ⊡
-        ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ ⊡
-        ♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙
-        ♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖
-
-    Returns:
-        list: Nested list of Entitys that represents the chess board.
-    """
-    board = []
-
-    board.append([
-        Rook((0, 0), "black"), 
-        Knight((1, 0), "black"), 
-        Bishop((2, 0), "black"), 
-        Queen((3, 0), "black"), 
-        King((4, 0), "black"), 
-        Bishop((5, 0), "black"), 
-        Knight((6, 0), "black"), 
-        Rook((7, 0), "black"),
-    ])
-    board.append([Pawn((i, 1), "black") for i in range(8)])
-
-    for i in range(4):
-        board.append([Empty((j, i + 2)) for j in range(8)])
-
-    board.append([Pawn((i, 6), "white") for i in range(8)])
-    board.append([
-        Rook((0, 7), "white"), 
-        Knight((1, 7), "white"), 
-        Bishop((2, 7), "white"), 
-        Queen((3, 7), "white"), 
-        King((4, 7), "white"), 
-        Bishop((5, 7), "white"), 
-        Knight((6, 7), "white"), 
-        Rook((7, 7), "white"),
-    ])
-    
-    return board
-
-def empty_board(self) -> List[List[Type[Entity]]]:
-    """Create a nested list of Entitys that represents an empty chess board.
-
-    Note:
-        The chess board is build with the position 
-        'a8' at the coordinate (0, 0) (h1 --> (7, 7)).
-        Reminder: Coordinates have to be translated!
-
-    Example:
-        >>> board = Board()
-        >>> board.board = board.empty_board()
-        >>> print(board.show())
-        ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ 
-        ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ ⊡
-        ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ ⊡
-        ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ ⊡
-        ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ ⊡
-        ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ ⊡
-        ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ ⊡
-        ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ ⊡ ⊡
-
-    Returns:
-        list: Nested list of Entitys that represents the chess board.
-    """
-    board = []
-
-    for i in range(8):
-        board.append([Empty((j, i)) for j in range(8)])
-
-    return board
+from typing import List
 
 
 class Boundary:
@@ -129,10 +25,10 @@ class Boundary:
         False
     """
     def __init__(self, min: int, max: int):
-        self.__min = min
-        self.__max = max
+        self.min = min
+        self.max = max
     
-    def accepts(self, value: Union[int, Tuple[int]]) -> bool:
+    def accepts(self, value: Union[int, List[int]]) -> bool:
         """Check if the value is inside the boundary.
 
         Args:
@@ -151,39 +47,54 @@ class Boundary:
         False
         """
         if isinstance(value, int):
-            value = tuple(value)
+            value = [value]
 
-        in_boundary = [v in range(self.__min, self.__max) for v in value]
+        in_boundary = [v in range(self.min, self.max) for v in value]
 
         return all(in_boundary)
 
 
-def coord_to_json(coord: Union[List[Tuple[int, int]], Tuple[int, int]],
-                  as_list: bool = False) -> dict:
+def coord_to_dict(coord: Union[List[List[int]], List[int]], 
+                  as_list: bool = False) -> Union[List[dict], dict]:
     """Convert a coordinate into a JSON representation."""
-    if isinstance(coord, tuple):
+    json = []
+
+    # Check if list of coord is empty.
+    if not coord:
+        return json
+    
+    # Check if list of coord is passed as arg.
+    if isinstance(coord[0], int):
         coord = [coord]
 
-    json = []
     for x, y in coord:
         json.append({"x": x, "y": y})
 
+    # Return a single dict if conditions meet.
     if len(json) == 1 and not as_list:
         return json[0]
 
     return json
 
 
-def json_to_coord(json: dict, as_list: bool = False) -> Union[List[Tuple[int, int]], Tuple[int, int]]:
+def dict_to_coord(json: Union[List[dict], dict], 
+                  as_list: bool = False) -> Union[List[List[int]], List[int]]:
     """Convert a coordinate in JSON representation into the internal."""
+    coord = []
+
+    # Check if list of coord is empty.
+    if not json:
+        return coord
+
+    # Check if list of dict is passed as arg.
     if isinstance(json, dict):
         json = [json]
 
-    coord = []
     for i in json:
         x, y = i["x"], i["y"]
-        coord.append((x, y))
+        coord.append([x, y])
 
+    # Return a single coord if conditions meet.
     if len(coord) == 1 and not as_list:
         return coord[0]
 
@@ -197,27 +108,3 @@ def normalize(x: int) -> int:
     elif x < 0:
             return -1
     return 0
-
-
-def str_to_piece(self, type: str, coord: Tuple[int], player: str, whitelist: Union[None, set] = None) -> Type[Piece]:
-    """Return a piece via it's type and other params.
-
-    Args:
-        type (str): Name of the class of the `Piece` object. 
-        coord (:obj:`tuple` of :obj:`int`): Coordinate of the piece on board.
-        player (str): Name of the piece's player.
-        whitelist (:obj:`set` of str): Whitelist for piece types.
-
-    Returns:
-        piece: A default piece object of given type and coord as well as player.
-
-    Raises:
-        NotWhitelistedException: The given piece type is not whitelisted!
-    """
-    if whitelist and type not in whitelist:
-        raise NotWhitelistedException(f"The given piece type is not whitelisted! {type} not in {whitelist}")
-
-    switch = {"Pawn": Pawn, "Knight": Knight, "Bishop": Bishop,
-              "Rook": Rook, "Queen": Queen, "King": King}
-
-    return switch[type](coord, player)
---
+++ test/test_utils.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""Unittests for code in the utils module.
+
+This module contains code to test the content
+of the pycheese.core.utils module using pytest.
+
+Example:
+    To run the tests you can for example:
+        - Run the pytest command from the command line:
+            ..> pytest
+        - Run the tests.py file in the repos top-level:
+            ..> python tests.py
+"""
+
+
+from pycheese.core.utils import Boundary
+from pycheese.core.utils import coord_to_dict
+from pycheese.core.utils import dict_to_coord
+
+from test.utils import assert_obj_attr
+
+
+def test_boundary():
+    """Test the Boundary class function.
+
+    Check if the functions's behavoir is correct.
+    To do so initialize an instance of the Boundary class
+    and assert the functions output with different setups.
+    """
+    min, max = 0, 10
+    boundary = Boundary(min, max)
+
+    # Test the attributes.
+    assert_obj_attr(boundary, "min", min)
+    assert_obj_attr(boundary, "max", max)
+
+    # Test if a boundary behaves correctly with single int.
+    assert boundary.accepts(min)
+
+    for i in range(min, max):
+        assert boundary.accepts(i)
+
+    assert not boundary.accepts(max)
+
+    # Test if a boundary behaves correctly with list of int.
+    for i in range(min, max):
+        for j in range(min, max):
+            assert boundary.accepts([i, j])
+
+    assert not boundary.accepts([max, max])
+
+
+def test_coord_to_dict():
+    x, y = 0, 0
+
+    # Test conversion of single coord.
+    coord = [x, y]
+    dict = {"x": x, "y": y}
+
+    assert coord_to_dict(coord) == dict
+
+    # Test conversion of list of coord.
+    coord = [[x, y]]*2
+    dict = [dict, dict]
+
+    assert coord_to_dict(coord) == dict
+
+    # Test special case with empty list.
+    assert coord_to_dict([]) == []
+
+
+def test_dict_to_coord():
+    x, y = 0, 0
+
+    # Test conversion of single dict.
+    dict = {"x": x, "y": y}
+    coord = [x, y]
+
+    assert dict_to_coord(dict) == coord
+
+    # Test conversion of list of coord.
+    dict = [dict, dict]
+    coord = [[x, y]]*2
+    
+    assert dict_to_coord(dict) == coord
+
+    # Test special case with empty list.
+    assert dict_to_coord([]) == []
+
