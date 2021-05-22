5c376bb8b43efd7752afd472c41c1424876dd7b0
1ucky40nc3 <louisnwendler@gmail.com>
2021-05-23 00:04:09 +0200

    updated tests

--- test/test_board.py
+++ test/test_board.py
@@ -12,6 +12,7 @@ Example:
             ..> python tests.py
 """
 
+from pycheese.core.utils import dict_to_coord
 from typing import List
 from typing import Type
 
@@ -25,10 +26,17 @@ from test.utils import white_pieces
 from test.utils import black_pieces
 from test.utils import initial_board
 
-from test.cases.cases_board_to_json import case_initial_board
-from test.cases.cases_board_to_json import case_napolean_attack_board
+from pycheese.core.entity import Pawn
+from pycheese.core.entity import Knight
+from pycheese.core.entity import Bishop
+from pycheese.core.entity import Rook
+from pycheese.core.entity import Queen
+from pycheese.core.entity import King
 
-from test.cases.cases_board_move import case_napolean_attack
+from test.cases.cases_board_to_dict import case_initial_board
+from test.cases.cases_board_to_dict import case_napoleon_attack_board
+
+from test.cases.cases_board_move import case_napoleon_attack
 from test.cases.cases_board_move import case_castle_kingside
 from test.cases.cases_board_move import case_castle_queenside
 
@@ -47,18 +55,6 @@ def test_board():
     assert_obj_attr(board, "board", initial_board())
 
 
-def test_initial_board():
-    """Test a boards `get_player_pieces` function.
-
-    Check if the functions's behavoir is correct.
-    To do so initialize an instance of the Board class
-    and assert the functions output with different setups.
-    """
-    board = Board()
-
-    assert_obj_func(board, "initial_board", None, initial_board())
-
-
 def test_get_piece_options():
     """Test a boards `get_piece_options` function.
 
@@ -68,24 +64,45 @@ def test_get_piece_options():
     """
     board = Board()
 
-    test_cases = [
+    cases = [
         {
-            "piece_moves": ([], []),
-            "coord": {"x": 0, "y": 7}
+            "name": "white Pawn at a2 - with empty options",
+            "board": {
+                'state': 'ongoing', 
+                'player': 'white', 
+                'pieces': [
+                    {'type': 'Pawn', 'player': 'white', 'coord': {'x': 0, 'y': 6}, 'options': {'moves': [], 'others': []}, 'pinned': False, 'pinner': None}, 
+                    {'type': 'King', 'player': 'white', 'coord': {'x': 4, 'y': 7}, 'options': {'moves': [], 'others': []}, 'pinned': False, 'pinner': None}, 
+                    {'type': 'King', 'player': 'black', 'coord': {'x': 4, 'y': 0}, 'options': {'moves': [], 'others': []}, 'pinned': False, 'pinner': None}
+                ]
+            },
+            "piece_coord": {"x": 0, "y": 6},
+            "piece_options": ([[0, 5], [0, 4]], []),
         },
         {
-            "piece_moves": ([(0, 5), (0, 4)], []),
-            "coord": {"x": 0, "y": 6}
+            "name": "white Pawn at a2 - with options",
+            "board": {
+                'state': 'ongoing', 
+                'player': 'white', 
+                'pieces': [
+                    {'type': 'Pawn', 'player': 'white', 'coord': {'x': 0, 'y': 6}, 'options': {'moves': [[0, 5], [0, 4]], 'others': []}, 'pinned': False, 'pinner': None}, 
+                    {'type': 'King', 'player': 'white', 'coord': {'x': 4, 'y': 7}, 'options': {'moves': [], 'others': []}, 'pinned': False, 'pinner': None}, 
+                    {'type': 'King', 'player': 'black', 'coord': {'x': 4, 'y': 0}, 'options': {'moves': [], 'others': []}, 'pinned': False, 'pinner': None}
+                ]
+            },
+            "piece_coord": {"x": 0, "y": 6},
+            "piece_options": ([[0, 5], [0, 4]], []),
         }
     ]
 
-    for case in test_cases:
-        x, y = case["coord"]["x"], case["coord"]["y"]
+    for case in cases:
+        print(case["name"])
+        board.from_dict(case["board"])
 
-        piece = board.board[y][x]
-        board_piece_moves = board.get_piece_options(piece, (x, y))
+        x, y = dict_to_coord(case["piece_coord"])
+        piece = board.get()[y][x]
 
-        assert sorted(board_piece_moves) == sorted(case["piece_moves"])
+        assert sorted(board.get_piece_options(piece)) == sorted(case["piece_options"])
 
 
 def test_get_player_pieces():
@@ -97,22 +114,49 @@ def test_get_player_pieces():
     """
     board = Board()
 
-    # Test the function with the initial board for `white`. And no `board` parameter.
-    player_pieces = white_pawns() + white_pieces()
-    board_player_pieces = board.get_player_pieces("white")
+    cases = [
+        {
+            "name": "only kings on board",
+            "board": {
+                'state': 'ongoing', 
+                'player': 'white', 
+                'pieces': [
+                    {'type': 'King', 'player': 'white', 'coord': {'x': 4, 'y': 7}, 'options': {'moves': [], 'others': []}, 'pinned': False, 'pinner': None}, 
+                    {'type': 'King', 'player': 'black', 'coord': {'x': 4, 'y': 0}, 'options': {'moves': [], 'others': []}, 'pinned': False, 'pinner': None}
+                ]
+            },
+            "white_pieces": [King([4, 7], "white")],
+            "black_pieces": [King([4, 0], "black")]
+        },
+        {
+            "name": "only kings a one pawn per player",
+            "board": {
+                'state': 'ongoing', 
+                'player': 'white', 
+                'pieces': [
+                    {'type': 'Pawn', 'player': 'white', 'coord': {'x': 0, 'y': 6}, 'options': {'moves': [], 'others': []}, 'pinned': False, 'pinner': None}, 
+                    {'type': 'King', 'player': 'white', 'coord': {'x': 4, 'y': 7}, 'options': {'moves': [], 'others': []}, 'pinned': False, 'pinner': None},
+                    {'type': 'Pawn', 'player': 'black', 'coord': {'x': 0, 'y': 1}, 'options': {'moves': [], 'others': []}, 'pinned': False, 'pinner': None},
+                    {'type': 'King', 'player': 'black', 'coord': {'x': 4, 'y': 0}, 'options': {'moves': [], 'others': []}, 'pinned': False, 'pinner': None}
+                ]
+            },
+            "white_pieces": [Pawn([0, 6], "white"), King([4, 7], "white")],
+            "black_pieces": [Pawn([0, 1], "black"), King([4, 0], "black")]
+        },
+        {
+            "name": "initial board",
+            "board": Board().to_dict(),
+            "white_pieces": white_pawns() + white_pieces(),
+            "black_pieces": black_pawns() + black_pieces()
+        }
+    ]
 
-    assert set(player_pieces) == set(board_player_pieces)
+    for case in cases:
+        print(case["name"])
+        board.from_dict(case["board"])
 
-    # Test the function with the initial board for `black`. And no `board` parameter.
-    player_pieces = black_pawns() + black_pieces()
-    board_player_pieces = board.get_player_pieces("black")
-
-    assert set(player_pieces) == set(board_player_pieces)
-
-    # Test the function with the initial board for `black`. With `board` parameter.
-    board_player_pieces = board.get_player_pieces("black", board=board.board)
-
-    assert set(player_pieces) == set(board_player_pieces)
+        assert set(board.get_player_pieces("white")) == set(case["white_pieces"])
+        assert set(board.get_player_pieces("black")) == set(case["black_pieces"])
 
 
 def test_next_turn():
@@ -133,7 +177,7 @@ def test_next_turn():
     assert_obj_attr(board, "player", "white")
 
 
-def test_to_json():
+def test_to_dict():
     """Test a boards `to_json` function.
 
     Check if the functions's behavoir is correct.
@@ -147,46 +191,64 @@ def test_to_json():
     for case in cases:
         board = Board()
 
-        assert board.to_json() == case
+        assert board.to_dict() == case
 
 
-def test_from_json():
-    """Test a boards `from_json` function.
+def test_from_dict():
+    """Test a boards `from_dict` function.
 
     Check if the functions's behavoir is correct.
     To do so initialize an instance of the Board class
     and assert the functions output with different setups.
     """
     cases = [
-        case_initial_board(),
-        case_napolean_attack_board(),
+        {
+            "name": "initial board - from board", 
+            "board": Board().to_dict()
+        },
+        {
+            "name": "initial board - from case", 
+            "board": case_initial_board()
+        },
     ]
     
     for case in cases:
-        board = Board(case)
-        assert board.to_json() == case
+        print(case["name"])
 
-        board.from_json(case)
-        assert board.to_json() == case
+        board = Board(case["board"])
+        assert board.to_dict() == case["board"]
+
+        board.from_dict(case["board"])
+        assert board.to_dict() == case["board"]
 
 
 def test_move():
-    """Test the boards `move` funtion.
+    """Test the boards `move` function.
 
     Check if the functions's behavoir is correct.
     To do so initialize an instance of the Board class
     and assert the functions output with different setups.
     """
     cases = [
-        case_napolean_attack(),
-        case_castle_kingside(),
-        case_castle_queenside(),
+        {
+            "name": "napoleon attack", 
+            "moves": case_napoleon_attack()
+        },
+        {
+            "name": "castle kingside",
+            "moves": case_castle_kingside(),
+        },
+        {
+            "name": "castle queenside",
+            "moves": case_castle_queenside(),
+        }
     ]
 
     for case in cases:
+        print(case["name"])
         board = Board()
 
-        for move in case:
+        for move in case["moves"]:
             source_coord = move["source_coord"]
             target_coord = move["target_coord"]
 
