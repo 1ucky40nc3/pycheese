f10899f750c738dfbdc0102cc1441522f34eb886
1ucky40nc3 <louisnwendler@gmail.com>
2021-05-23 00:03:20 +0200

    updated test cases

--- test/cases/cases_board_move.py
+++ test/cases/cases_board_move.py
@@ -6,7 +6,7 @@ of the pycheese.core.board module using pytest.
 """
 
 
-def case_napolean_attack() -> dict:
+def case_napoleon_attack() -> dict:
     """Test case for the boards `move` funtion.
     
     To test the function the follow chess game will be played:
@@ -14,80 +14,80 @@ def case_napolean_attack() -> dict:
     """
     return [
         {
-            "source_coord": (4, 6),
-            "target_coord": (4, 4),
+            "source_coord": [4, 6],
+            "target_coord": [4, 4],
             "promotion_target": None,
             "output": {
                 'state': 'ongoing', 
                 'source_coord': {'x': 4, 'y': 6}, 
                 'target_coord': {'x': 4, 'y': 4}, 
-                'event': {'type': None, 'extra': None}
+                'event': {'extra': 'unique', 'type': 'move'}
             }
         },
         {
-            "source_coord": (4, 1),
-            "target_coord": (4, 3),
+            "source_coord": [4, 1],
+            "target_coord": [4, 3],
             "promotion_target": None,
             "output": {
                 'state': 'ongoing', 
                 'source_coord': {'x': 4, 'y': 1}, 
                 'target_coord': {'x': 4, 'y': 3}, 
-                'event': {'type': None, 'extra': None}
+                'event': {'extra': 'unique', 'type': 'move'}
             }
         },
         {
-            "source_coord": (3, 7),
-            "target_coord": (5, 5),
+            "source_coord": [3, 7],
+            "target_coord": [5, 5],
             "promotion_target": None,
             "output": {
                 'state': 'ongoing', 
                 'source_coord': {'x': 3, 'y': 7}, 
                 'target_coord': {'x': 5, 'y': 5}, 
-                'event': {'type': None, 'extra': None}
+                'event': {'extra': 'unique', 'type': 'move'}
             }
         },
         {
-            "source_coord": (1, 0),
-            "target_coord": (2, 2),
+            "source_coord": [1, 0],
+            "target_coord": [2, 2],
             "promotion_target": None,
             "output": {
                 'state': 'ongoing', 
                 'source_coord': {'x': 1, 'y': 0}, 
                 'target_coord': {'x': 2, 'y': 2}, 
-                'event': {'type': None, 'extra': None}
+                'event': {'extra': 'unique', 'type': 'move'}
             }
         },
         {
-            "source_coord": (5, 7),
-            "target_coord": (2, 4),
+            "source_coord": [5, 7],
+            "target_coord": [2, 4],
             "promotion_target": None,
             "output": {
                 'state': 'ongoing', 
                 'source_coord': {'x': 5, 'y': 7}, 
                 'target_coord': {'x': 2, 'y': 4}, 
-                'event': {'type': None, 'extra': None}
+                'event': {'extra': 'unique', 'type': 'move'}
             }
         },
         {
-            "source_coord": (3, 1),
-            "target_coord": (3, 2),
+            "source_coord": [3, 1],
+            "target_coord": [3, 2],
             "promotion_target": None,
             "output": {
                 'state': 'ongoing', 
                 'source_coord': {'x': 3, 'y': 1}, 
                 'target_coord': {'x': 3, 'y': 2}, 
-                'event': {'type': None, 'extra': None}
+                'event': {'extra': 'unique', 'type': 'move'}
             }
         },
         {
-            "source_coord": (5, 5),
-            "target_coord": (5, 1),
+            "source_coord": [5, 5],
+            "target_coord": [5, 1],
             "promotion_target": None,
             "output": {
                 'state': 'checkmate', 
                 'source_coord': {'x': 5, 'y': 5}, 
                 'target_coord': {'x': 5, 'y': 1}, 
-                'event': {'type': 'captures', 'extra': None}
+                'event': {'type': 'captures', 'extra': "unique"}
             }
         },
     ]
@@ -101,74 +101,74 @@ def case_castle_kingside() -> dict:
     """
     return [
         {
-            "source_coord": (6, 7),
-            "target_coord": (5, 5),
+            "source_coord": [6, 7],
+            "target_coord": [5, 5],
             "promotion_target": None,
             "output": {
                 'state': 'ongoing', 
                 'source_coord': {'x': 6, 'y': 7}, 
                 'target_coord': {'x': 5, 'y': 5}, 
-                'event': {'type': None, 'extra': None}
+                'event': {'extra': 'unique', 'type': 'move'}
             }
         },
         {
-            "source_coord": (6, 0),
-            "target_coord": (5, 2),
+            "source_coord": [6, 0],
+            "target_coord": [5, 2],
             "promotion_target": None,
             "output": {
                 'state': 'ongoing', 
                 'source_coord': {'x': 6, 'y': 0}, 
                 'target_coord': {'x': 5, 'y': 2}, 
-                'event': {'type': None, 'extra': None}
+                'event': {'extra': 'unique', 'type': 'move'}
             }
         },
         {
-            "source_coord": (4, 6),
-            "target_coord": (4, 5),
+            "source_coord": [4, 6],
+            "target_coord": [4, 5],
             "promotion_target": None,
             "output": {
                 'state': 'ongoing', 
                 'source_coord': {'x': 4, 'y': 6}, 
                 'target_coord': {'x': 4, 'y': 5}, 
-                'event': {'type': None, 'extra': None}
+                'event': {'extra': 'unique', 'type': 'move'}
             }
         },
         {
-            "source_coord": (4, 1),
-            "target_coord": (4, 2),
+            "source_coord": [4, 1],
+            "target_coord": [4, 2],
             "promotion_target": None,
             "output": {
                 'state': 'ongoing', 
                 'source_coord': {'x': 4, 'y': 1}, 
                 'target_coord': {'x': 4, 'y': 2}, 
-                'event': {'type': None, 'extra': None}
+                'event': {'extra': 'unique', 'type': 'move'}
             }
         },
         {
-            "source_coord": (5, 7),
-            "target_coord": (4, 6),
+            "source_coord": [5, 7],
+            "target_coord": [4, 6],
             "promotion_target": None,
             "output": {
                 'state': 'ongoing', 
                 'source_coord': {'x': 5, 'y': 7}, 
                 'target_coord': {'x': 4, 'y': 6}, 
-                'event': {'type': None, 'extra': None}
+                'event': {'extra': 'unique', 'type': 'move'}
             }
         },
         {
-            "source_coord": (5, 0),
-            "target_coord": (4, 1),
+            "source_coord": [5, 0],
+            "target_coord": [4, 1],
             "promotion_target": None,
             "output": {
                 'state': 'ongoing', 
                 'source_coord': {'x': 5, 'y': 0}, 
                 'target_coord': {'x': 4, 'y': 1}, 
-                'event': {'type': None, 'extra': None}
+                'event': {'extra': 'unique', 'type': 'move'}
             }
         },
         {
-            "source_coord": (4, 7),
-            "target_coord": (6, 7),
+            "source_coord": [4, 7],
+            "target_coord": [6, 7],
             "promotion_target": None,
             "output": {
                 'state': 'ongoing', 
@@ -178,8 +178,8 @@ def case_castle_kingside() -> dict:
             }
         },
         {
-            "source_coord": (4, 0),
-            "target_coord": (6, 0),
+            "source_coord": [4, 0],
+            "target_coord": [6, 0],
             "promotion_target": None,
             "output": {
                 'state': 'ongoing', 
@@ -199,96 +199,96 @@ def case_castle_queenside() -> dict:
     """
     return [
         {
-            "source_coord": (1, 7),
-            "target_coord": (2, 5),
+            "source_coord": [1, 7],
+            "target_coord": [2, 5],
             "promotion_target": None,
             "output": {
                 'state': 'ongoing', 
                 'source_coord': {'x': 1, 'y': 7}, 
                 'target_coord': {'x': 2, 'y': 5}, 
-                'event': {'type': None, 'extra': None}
+                'event': {'extra': 'unique', 'type': 'move'}
             }
         },
         {
-            "source_coord": (1, 0),
-            "target_coord": (2, 2),
+            "source_coord": [1, 0],
+            "target_coord": [2, 2],
             "promotion_target": None,
             "output": {
                 'state': 'ongoing', 
                 'source_coord': {'x': 1, 'y': 0}, 
                 'target_coord': {'x': 2, 'y': 2}, 
-                'event': {'type': None, 'extra': None}
+                'event': {'extra': 'unique', 'type': 'move'}
             }
         },
         {
-            "source_coord": (3, 6),
-            "target_coord": (3, 5),
+            "source_coord": [3, 6],
+            "target_coord": [3, 5],
             "promotion_target": None,
             "output": {
                 'state': 'ongoing', 
                 'source_coord': {'x': 3, 'y': 6}, 
                 'target_coord': {'x': 3, 'y': 5}, 
-                'event': {'type': None, 'extra': None}
+                'event': {'extra': 'unique', 'type': 'move'}
             }
         },
         {
-            "source_coord": (3, 1),
-            "target_coord": (3, 2),
+            "source_coord": [3, 1],
+            "target_coord": [3, 2],
             "promotion_target": None,
             "output": {
                 'state': 'ongoing', 
                 'source_coord': {'x': 3, 'y': 1}, 
                 'target_coord': {'x': 3, 'y': 2}, 
-                'event': {'type': None, 'extra': None}
+                'event': {'extra': 'unique', 'type': 'move'}
             }
         },
         {
-            "source_coord": (2, 7),
-            "target_coord": (4, 5),
+            "source_coord": [2, 7],
+            "target_coord": [4, 5],
             "promotion_target": None,
             "output": {
                 'state': 'ongoing', 
                 'source_coord': {'x': 2, 'y': 7}, 
                 'target_coord': {'x': 4, 'y': 5}, 
-                'event': {'type': None, 'extra': None}
+                'event': {'extra': 'unique', 'type': 'move'}
             }
         },
         {
-            "source_coord": (2, 0),
-            "target_coord": (4, 2),
+            "source_coord": [2, 0],
+            "target_coord": [4, 2],
             "promotion_target": None,
             "output": {
                 'state': 'ongoing', 
                 'source_coord': {'x': 2, 'y': 0}, 
                 'target_coord': {'x': 4, 'y': 2}, 
-                'event': {'type': None, 'extra': None}
+                'event': {'extra': 'unique', 'type': 'move'}
             }
         },
         {
-            "source_coord": (3, 7),
-            "target_coord": (3, 6),
+            "source_coord": [3, 7],
+            "target_coord": [3, 6],
             "promotion_target": None,
             "output": {
                 'state': 'ongoing', 
                 'source_coord': {'x': 3, 'y': 7}, 
                 'target_coord': {'x': 3, 'y': 6}, 
-                'event': {'type': None, 'extra': None}
+                'event': {'extra': 'unique', 'type': 'move'}
             }
         },
         {
-            "source_coord": (3, 0),
-            "target_coord": (3, 1),
+            "source_coord": [3, 0],
+            "target_coord": [3, 1],
             "promotion_target": None,
             "output": {
                 'state': 'ongoing', 
                 'source_coord': {'x': 3, 'y': 0}, 
                 'target_coord': {'x': 3, 'y': 1}, 
-                'event': {'type': None, 'extra': None}
+                'event': {'extra': 'unique', 'type': 'move'}
             }
         },
         {
-            "source_coord": (4, 7),
-            "target_coord": (2, 7),
+            "source_coord": [4, 7],
+            "target_coord": [2, 7],
             "promotion_target": None,
             "output": {
                 'state': 'ongoing', 
@@ -298,8 +298,8 @@ def case_castle_queenside() -> dict:
             }
         },
         {
-            "source_coord": (4, 0),
-            "target_coord": (2, 0),
+            "source_coord": [4, 0],
+            "target_coord": [2, 0],
             "promotion_target": None,
             "output": {
                 'state': 'ongoing', 
